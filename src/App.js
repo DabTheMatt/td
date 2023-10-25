dc9d1f814e8903c5c6d606d72329fba5d05db403
DabTheMatt <inspiromaciek@gmail.com>
2023-10-25 21:05:22 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[toDos, setToDos] = useState([]);
+  const[elementName, setElementName] = useState('');
+
+  function handleAdd(e, name) {
+    e.preventDefault();
+    console.log('add', name);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <h2>To Do</h2>
+      <AddToDo 
+        add={handleAdd}
+        element={elementName}
+      />
+      <hr className='line-divider'/>
+      <ToDoList 
+        todos={toDos}
+      />
+      <hr className='line-divider'/>
+      <DoneList 
+        todos={toDos}
+      />
     </div>
   );
 }
+
+function ToDoList() {
+  return (
+    <div className='list-container'>
+      To Do
+    </div>
+  )
+}
+
+function DoneList() {
+  return (
+    <div className='list-container'>
+      Done
+    </div>
+  )
+}
+
+function AddToDo(props) {
+
+  const[toDoName, setToDoName] = useState('');
+
+  return (
+    <>
+    <form onSubmit={(e, toDoName)=>props.add(e, toDoName)}>
+      <input type="text" onChange={(e) => setToDoName(e.target.value)}/>
+      <button type="submit">Add something To Do</button>
+    </form>
+    </>
+  )
+}
+
+function Element() {
+  return (
+    <div className='element-container'>
+     <h3></h3>
+     <input type="checkbox"/>
+     <p>del</p>
+    </div>
+  )
+}
+
 
 export default App;
